8babfbc6287c7aa90adbbea1c6b9a85e995c8351
dodyprakarsa <dontris@gmail.com>
2021-08-18 01:33:56 +0000

    add mongodb connection

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require ('express');
-const app = express();
+const mongoose = require ('mongoose');
 require('dotenv').config();
 
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => console.log("Index is Connected"));
+
+const app = express();
+//express route
 app.get("/", (req, res) => {
-    res.send("hello from me");
+    res.send("hello again");
 });
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`server is running on ${port}`);
+    console.log(`server is running on port ${port}`);
 });
